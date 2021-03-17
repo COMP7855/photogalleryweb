baa457ec6e113ee9ab9132d0ef1e0812c24dc1c3
Mephistoss <makinaleksey@gmail.com>
2021-03-17 11:17:22 -0700

    wow
    wow!

--- WEB-INF/classes/GalleryServlet.java
+++ WEB-INF/classes/GalleryServlet.java
@@ -1,30 +1,13 @@
-import javax.servlet.http.*;
-import javax.servlet.*;
 import java.io.*;
-
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import com.oreilly.servlet.MultipartRequest;
 public class GalleryServlet extends HttpServlet {
-  
-  public void doGet(HttpServletRequest request,
-      HttpServletResponse response)
-      throws ServletException, IOException {
-    response.setContentType("text/html");
-    PrintWriter out = response.getWriter();
-    
-    out.println("<html>\n"+
-    "<body>\n"+
-    "<h1>Photogallery app</h1>\n"+
-    "<h2>Gallery view</h2>\n"+
-    "<img src=\"images/a.jpg\" alt=\"a image\" width=\"830\" height=\"415\">\n"+
-    "<p>Edit caption</p>\n"+
-    "<button type=\"button\" onclick=\"alert('Right')\">Snap</button>\n"+
-    "<p></p>\n"+
-    "<button type=\"button\" onclick=\"alert('Left')\">Left</button>\n"+
-    "<button type=\"button\" onclick=\"alert('Right')\">Right</button>\n"+
-    "<p></p>\n"+
-    "<button type=\"button\" onclick=\"alert('Search')\">Search</button>\n"+
-    "<p>Time textbox</p>\n"+
-    "<p>Location textbox</p>\n"+
-    "</body>\n"+
-    "</html>");
-  }
+public void doPost(HttpServletRequest request, HttpServletResponse response) 
+throws ServletException, IOException {
+     response.setContentType("text/html");
+     PrintWriter out = response.getWriter();
+     MultipartRequest m = new MultipartRequest(request,"c:/tomcat/webapps/photogalleryweb/images");
+     out.print("successfully uploaded");
+   }
 }
