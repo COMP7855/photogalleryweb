256f0054aa553e4aee2e5b1c5e62721a1a550b1f
JoshPenner <ircobra@gmail.com>
2021-03-18 15:32:03 -0700

    Added directory and upload servlets

--- WEB-INF/classes/UploadServlet.java
+++ WEB-INF/classes/UploadServlet.java
@@ -2,7 +2,7 @@ import java.io.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import com.oreilly.servlet.MultipartRequest;
-public class GalleryServlet extends HttpServlet {
+public class UploadServlet extends HttpServlet {
 public void doPost(HttpServletRequest request, HttpServletResponse response) 
 throws ServletException, IOException {
      response.setContentType("text/html");
