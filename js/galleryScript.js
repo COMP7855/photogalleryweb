0f266b34db2a1185c85e9a975186de239bfd0f6b
JoshPenner <ircobra@gmail.com>
2021-03-22 19:44:39 -0700

    Cleanup & moved script to .js file

---
+++ js/galleryScript.js
@@ -0,0 +1,124 @@
+index = 0;
+var photoArray;
+var photoArrayFiltered;
+loadDoc();
+
+function loadDoc() 
+{
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() 
+    {
+    if (this.readyState == 4 && this.status == 200) 
+    {
+        var photoList = this.responseText;
+        photoArray = photoList.split(",");
+            photoArray[0] = photoArray[0].substring(4);
+
+        document.getElementById("photoListText").innerHTML = photoArray;
+        
+    }
+    };
+    xhttp.open("GET", "directory", true);
+    xhttp.send();
+    
+    filterList();
+    updateCaption();
+    document.getElementById("ImageView").src = "images/" + photoArrayFiltered[index];
+}
+
+function checkKeywords(photoName) 
+{
+    var url = new URL(window.location.href);
+    keywords = url.searchParams.get("keywords");
+    document.getElementById("keywordsText").innerHTML = keywords;
+    
+    if(keywords != null)
+    {
+    var containsKeywords = photoName.includes(keywords);
+    }
+    else
+    {
+    var containsKeywords = true;
+    }
+    return containsKeywords;
+}
+
+function checkTime(photoName) 
+{
+    var url = new URL(window.location.href);
+    start_time_str = url.searchParams.get("start_time");
+    end_time_str = url.searchParams.get("end_time");
+    
+    start_time = new Date(start_time_str);
+    end_time = new Date(end_time_str);
+
+    document.getElementById("startTimeText").innerHTML = start_time;
+    document.getElementById("endTimeText").innerHTML = end_time;
+
+    photoAttributes = photoName.split("_");
+    photoTime = new Date(photoAttributes[1]);
+
+    if(start_time_str.length >= 1 && end_time_str.length >= 1)
+    {
+    var withinStartEndTime = ((photoTime > start_time) && (photoTime < end_time));
+    }
+    else
+    {
+    var withinStartEndTime = true;
+    }
+    return withinStartEndTime;
+}
+
+function filterList()
+{
+    photoArrayFiltered = photoArray.filter(checkKeywords);
+    photoArrayFiltered = photoArrayFiltered.filter(checkTime);
+    document.getElementById("filteredListText").innerHTML = photoArrayFiltered;
+}
+
+function prevButton()
+{
+    if (index <= 0)
+    {
+    index = 0;
+    }
+    else
+    {
+    index--;
+    }
+    
+    document.getElementById("ImageView").src = "images/" + photoArrayFiltered[index];
+    document.getElementById("index").innerHTML = index;
+    document.getElementById("currentPhoto").innerHTML = "images/" + photoArrayFiltered[index];
+    updateCaption();
+}
+
+function nextButton()
+{
+    if (index >= photoArrayFiltered.length -1)
+    {
+    index = photoArrayFiltered.length -1;
+    }
+    else
+    {
+    index++;
+    }
+
+    document.getElementById("ImageView").src = "images/" + photoArrayFiltered[index];
+    document.getElementById("index").innerHTML = index;
+    document.getElementById("currentPhoto").innerHTML = "images/" + photoArrayFiltered[index];
+    updateCaption();
+}
+
+function updateCaption()
+{
+    photoAttributes = photoArrayFiltered[index].split("_");
+    photoCaption = photoAttributes[0];
+    photoTime = photoAttributes[1];
+    photoLocation = photoAttributes[2];
+
+    document.getElementById("oldCaption").value = photoArray[index];
+    document.getElementById("timeText").innerHTML = photoTime;
+    document.getElementById("locationText").innerHTML = photoLocation;
+    document.getElementById("currentCaption").innerHTML = photoCaption;
+}
